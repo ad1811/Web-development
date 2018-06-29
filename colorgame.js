2087f6d6abc8ba7a6af0461238fd2d55b94ce267
Aditya Singh Chauhan <karan19972002@gmail.com>
2018-06-29 16:13:59 +0530

    todo by jquery

--- colorgame.js
+++ colorgame.js
@@ -1,5 +1,5 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
@@ -14,27 +14,56 @@ easyBtn.addEventListener("click",function()
 {
       easyBtn.classList.add("selected");
       hardBtn.classList.remove("selected");
+      numSquares = 3;
+      colors = generateRandomColors(numSquares);
+      pickedColor = pickColor();
+      colorDisplay.textContent = pickedColor;
+      for(var i=0;i<squares.length;i++)
+      {
+      	if(colors[i])
+      	{
+      		squares[i].style.background = colors[i];
+      	}
+      	else
+      	{
+      		squares[i].style.display = "none";
+      	}
+      }
+      
 })
 
 hardBtn.addEventListener("click",function()
 {
 	easyBtn.classList.remove("selected");
       hardBtn.classList.add("selected");
+      numSquares = 6;
+      colors = generateRandomColors(numSquares);
+      pickedColor = pickColor();
+      colorDisplay.textContent = pickedColor;
+      for(var i=0;i<squares.length;i++)
+      {
+      
+      		squares[i].style.background = colors[i];
+      	    squares[i].style.display = "block";
+      	
+      }
+ 
 
 })
 
 resetButton.addEventListener("click",function()
 {
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
     for(var i=0;i<squares.length;i++)
 	{
 		squares[i].style.background = colors[i];
 	}
-	h1.style.backgroundColor = "#232323";
+	h1.style.backgroundColor = "steelblue";
+	resetButton.textContent = "New Colors";
+	messageDisplay.textContent = "";
 })
-
 
 colorDisplay.textContent = pickedColor;
 for(var i=0;i<squares.length;i++)
